004f43e4553ebdf53c40811eac64d186c36d8bd8
elenh <yisiwings@163.com>
2021-08-12 22:50:08 +0800

    build: pkg

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
+import typescript from '@rollup/plugin-typescript'
+// import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+// import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
-      name: "VueCompositionWrapper",
+      name: 'VueCompositionWrapper',
       file: pkg.main,
-      format: "umd",
+      format: 'umd',
     },
     {
       file: pkg.module,
-      format: "es",
+      format: 'es',
     },
   ],
   plugins: [
     nodeResolve(),
-    commonjs(),
+    // commonjs(),
     typescript(), // so Rollup can convert TypeScript to JavaScript
-    terser(),
+    // terser(),
   ],
-};
+}
